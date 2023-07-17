174cc1b5716bfaf229c125affbbf4fb57d580aa9
AyoubMS <chahayb563@gmail.com>
2023-07-17 13:04:37 +0200

    add initial setup

--- api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.ApiOverview),
-    path('courses/', views.get_courses, name="ViewCourses"),
-    path('courses/add/', views.add_course, name="AddCourse"),
-    path('courses/<str:id>/update', views.update_course, name="UpdateCourse"),
-]
